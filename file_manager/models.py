3b7b9a4fc88d1b9fbf5210fe449e13f051ea9c0b
hozbig <hozbig@gmail.com>
2022-08-03 23:56:57 +0430

    file manager model

--- file_manager/models.py
+++ file_manager/models.py
@@ -1,3 +1,25 @@
+from account.models import User
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class FileManager(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="files/")
+    format = models.CharField(max_length=50, blank= True)
+    size = models.CharField(('size(bytes)'), max_length=50, blank= True)
+
+    divide_to = models.IntegerField(
+        validators=[MinValueValidator(3)],
+        default=3,
+    )
+
+    created_time = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.file} - {self.format} - {self.size}'
+
+    def get_absolute_url(self):
+        return reverse("file_manager:upload")
