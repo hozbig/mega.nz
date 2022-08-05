a82dc9a7bf969f685d67bf46eb2f073dcb74eda5
hozbig <hozbig@gmail.com>
2022-08-05 22:21:35 +0430

    premium check in template and server

--- file_manager/mixins.py
+++ file_manager/mixins.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 class FormValidMixin():
     def form_valid(self, form):
         user = User.objects.get(username=self.request.user.username)
-        if user.user_uploaded_volume <= 2e+9:  # 2e+9 == 2000000000
+        if user.user_uploaded_volume <= 2e+9 or user.premium:  # 2e+9 == 2000000000
             self.obj = form.save(commit=False)
             self.obj.owner = self.request.user
             self.obj.format = Path(str(form.cleaned_data['file'])).suffix
