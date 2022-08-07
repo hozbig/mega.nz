75cbd765e53810dfff5172aa8982b58933e850f8
hozbig <hozbig@gmail.com>
2022-08-07 04:43:54 +0430

    new template run succuss fully. base.html, custom js file, templatetags, view.

---
+++ file_manager/templatetags/tag_library.py
@@ -0,0 +1,12 @@
+import os
+
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def filename(value):
+    try:
+        return os.path.basename(value.file.name)
+    except:
+        return "NoFile!"
--- file_manager/views.py
+++ file_manager/views.py
@@ -10,3 +10,9 @@ class UploadFile(FormValidMixin, CreateView):
     model = FileManager
     template_name = 'file_manager/upload.html'
     fields = ['file', 'divide_to', 'size']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["uploaded_files"] = FileManager.objects.filter(owner=self.request.user)
+        return context
+    
